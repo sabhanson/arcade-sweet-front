fad0c96cce985ec3934f83634871645a1c62ee68
Megan McLean <425megs@gmail.com>
2022-03-10 14:43:10 -0800

    minor styling/formatting

--- src/components/pages/Highscores.js
+++ src/components/pages/Highscores.js
@@ -5,11 +5,14 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import Box from '@mui/material/Box';
 
 const styles = {
   tableContainer: {
     background: "#1B998B",
-    maxWidth: "80vw",
+    // maxWidth: "80vw",
     border: "5px double white",
     padding: "10px",
     boxShadow: "0px 0px 20px black",
@@ -27,6 +30,27 @@ const styles = {
   tableHeader: { color: "#F46036", fontSize: "20px" },
 };
 
+// const [value, setValue] = React.useState('one');
+
+// const handleChange = (event, newValue) => {
+//   setValue(newValue);
+// };
+
+// return (
+//   <Box sx={{ width: '100%' }}>
+//     <Tabs
+//       value={value}
+//       onChange={handleChange}
+//       textColor="secondary"
+//       indicatorColor="secondary"
+//       aria-label="secondary tabs example"
+//     >
+//       <Tab value="one" label="Card Matching" />
+//       <Tab value="two" label="Wordle" />
+//     </Tabs>
+//   </Box>
+// );
+
 export function Highscores() {
   const rows = [
     createData("username", 100),
@@ -41,11 +65,10 @@ export function Highscores() {
   }
 
   return (
-    <div>
-      <div className="d-flex justify-content-center">
-        {/* <h1 className='col-12'>Snake</h1> */}
+    <div className="d-flex justify-content-center row">
+      <div className="col-5">
         <TableContainer style={styles.tableContainer}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <Table aria-label="simple table">
             <TableHead>
               <p style={styles.title}>Card Matching</p>
               <TableRow>
@@ -73,10 +96,9 @@ export function Highscores() {
           </Table>
         </TableContainer>
       </div>
-      <div className="d-flex justify-content-center">
-        {/* <h1 className='col-12'>Snake</h1> */}
-        <TableContainer style={styles.tableContainer}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <div className="col-5">
+        <TableContainer  style={styles.tableContainer}>
+          <Table aria-label="simple table">
             <TableHead>
               <p style={styles.title}>Wordle</p>
               <TableRow>
@@ -103,10 +125,9 @@ export function Highscores() {
             </TableBody>
           </Table>
         </TableContainer>
-      </div>{" "}
-      <div className="d-flex justify-content-center">
-        {/* <h1 className='col-12'>Snake</h1> */}
-        <TableContainer style={styles.tableContainer}>
+      </div>
+      {/* <div className="d-flex justify-content-center"> */}
+        {/* <TableContainer style={styles.tableContainer}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <p style={styles.title}>TBD</p>
@@ -133,8 +154,8 @@ export function Highscores() {
               ))}
             </TableBody>
           </Table>
-        </TableContainer>
-      </div>
+        </TableContainer> */}
+      {/* </div> */}
     </div>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 import CardMatch from "./CardMatch";
-import Snake from "./Snake";
+import Guides from "./GameGuide";
 import Wordle from "./Wordle";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "./styles/Home.css"
+import CloseIcon from '@mui/icons-material/Close';
 
 export function Home() {
   const [currentPage, setCurrentPage] = useState("Home");
+  const [display, setDisplay] = useState(true);
 
   const renderPage = () => {
     if (currentPage === "CardMatch") {
       return <CardMatch />;
     }
-    if (currentPage === "Snake Game") {
-      return <Snake />;
+    if (currentPage === "GameGuide") {
+      return <Guides />;
     }
     if (currentPage === "Wordle") {
       return <Wordle />;
@@ -22,14 +31,63 @@ export function Home() {
 
   return (
     <div>
-      <h1>this is the Home page</h1>
-      {/* <CardMatch /> */}
-      <button onClick={() => handlePageChange("CardMatch")}>
-        open card match
-      </button>
-      <button onClick={() => handlePageChange("Snake Game")}>open snake</button>
-      <button onClick={() => handlePageChange("Wordle")}>open wordle</button>
+      <h1>Welcome to Arcade Sweet! Choose a game below to get started :)</h1>
       {renderPage()}
+
+      <Card className={
+          display
+            ? ""
+            : "hidden"
+        }>
+        <CardMedia
+          component="img"
+          height="140"
+          image="public/matchgame.png"
+          alt="card matching screenshot"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Card Matching
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Match your favorite La Croix flavors in the fewest moves!
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => {
+            handlePageChange("CardMatch"); 
+            setDisplay(false);
+          }}>Play Now!</Button>
+          <Button size="small" onClick={() => handlePageChange("GameGuide")}>How to Play</Button>
+        </CardActions>
+      </Card>
+      <Card className={
+          display
+            ? ""
+            : "hidden"
+        }>
+        <CardMedia
+          component="img"
+          height="140"
+          image="public/wordlegame.png"
+          alt="wordle screenshot"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Wordle
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Guess the word in less than 6 tries!
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => {
+            handlePageChange("Wordle"); 
+            setDisplay(false);
+          }}>Play now!</Button>
+          <Button size="small" onClick={() => handlePageChange("GameGuide")}>How to Play</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
