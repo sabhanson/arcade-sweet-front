143ebb2a41caf18893e5fe0a460e3cd9df12f5fa
Sabrina Hanson <sabhanson7@gmail.com>
2022-03-09 22:36:42 -0800

    profile page

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,10 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+const styles = {
+  modal: {
+    background: "#1B998B",
+    textShadow: "2px 2px 3px #f46036",
+  },
+  input: {
+    borderRadius: "8px",
+  },
+  title: {
+    color: "white",
+    textAlign: "center",
+  },
+  button: {
+    background: "#d7263d",
+    margin: "10px",
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+  },
+  icon: {
+    color: "#F46036",
+    width: "30px",
+    height: "30px",
+  },
+  div: {
+    marginTop: "30px",
+    background: "#1B998B",
+    border: "5px double white",
+    padding: "10px",
+    boxShadow: "0px 0px 20px black",
+    borderRadius: "15px",
+  },
+  h1: {
+    textAlign: "center",
+    color: "white",
+    textShadow: "2px 2px 3px #f46036",
+    padding: "10px",
+  },
+};
 
 export function Profile() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div>
-      <h1>this is the Profile page</h1>
-    </div>
+    <>
+      <div className="d-flex justify-content-end">
+        <button onClick={handleShow} style={styles.button}>
+          <SettingsIcon style={styles.icon} />
+        </button>
+        <Modal show={show} onHide={handleClose}>
+          <div style={styles.modal}>
+            <Modal.Header closeButton>
+              <Modal.Title style={styles.title}>
+                Update Account Settings
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form className="d-flex row justify-content-center">
+                <input
+                  style={styles.input}
+                  className="col-12"
+                  placeholder="new username..."
+                ></input>
+                <input
+                  style={styles.input}
+                  className="col-12"
+                  placeholder="new email..."
+                ></input>
+                <input
+                  style={styles.input}
+                  className="col-12"
+                  type={"file"}
+                ></input>
+              </form>
+            </Modal.Body>
+            <Modal.Footer>
+              {/* need to add an onclick for the save changes button that submits a put request to change the user data */}
+              <Button variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </div>
+        </Modal>
+      </div>
+      <div className="d-flex justify-content-center">
+        <div style={styles.div} className="card col-8">
+          <h1 style={styles.h1}>My Highscores</h1>
+          <div></div>
+        </div>
+      </div>
+    </>
   );
 }
 
